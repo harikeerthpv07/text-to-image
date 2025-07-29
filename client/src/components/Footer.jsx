b0b1e0c03adbb17a94fea8046062dbec3dedcdfc
harikeerthpv07 <harikeerthpv07@gmail.com>
2025-07-29 13:20:00 +0530

    first

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="flex items-center justify-between gap-4 py-3 mt-20">
       <img src={assets.logo} alt="" width={150} />
       <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm">
-        Copyright @Aswin.dev | All right reserved.{" "}
+        Copyright @Harikeerth.dev | All right reserved.{" "}
       </p>
       <div className="flex gap-2.5 ">
         <img src={assets.facebook_icon} alt="" width={35} />
