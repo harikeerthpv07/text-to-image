4858ac526d4bba3c421ab2c3566d9755cfc367f4
harikeerthpv07 <harikeerthpv07@gmail.com>
2025-07-29 13:29:19 +0530

    cleaned

--- server/server.js
+++ server/server.js
@@ -1,38 +1,30 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import connectDB from "./config/mongodb.js";
-import userRouter from "./routes/userRoutes.js";
-import imageRouter from "./routes/imageRoutes.js";
+import dotenv from "dotenv";
+import connectDB from "../config/mongodb.js";
+import userRouter from "../routes/userRoutes.js";
+import imageRouter from "../routes/imageRoutes.js";
 
-const express = require("express");
-const cors = require("cors");
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-// Allow requests from frontend
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true, // if using cookies
+    credentials: true,
   })
 );
 
 app.use(express.json());
 
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
-module.exports = app;
+connectDB();
 
-app.use(express.json());
-app.use(cors());
-await connectDB();
-
-app.use("/api/user", userRouter);
-app.use("/api/image", imageRouter);
-
-app.get("/", (req, res) => res.send("API Working"));
-
-app.listen(PORT, () => console.log("Server running on port" + PORT));
+export default app;
